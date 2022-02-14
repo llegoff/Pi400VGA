7e4c5773b98fff1d091302f85a5a002180b3d8c4
Laurent Le Goff <laurent.le.goff@free.fr>
2022-02-15 00:25:43 +0100

    Update rpi-dpidac.c

--- overlays/rpi-dpidac.c
+++ overlays/rpi-dpidac.c
@@ -210,7 +210,8 @@ static int dpidac_attach(struct drm_bridge *bridge, enum drm_bridge_attach_flags
         bus_format = MEDIA_BUS_FMT_RGB666_1X24_CPADHI;
     }
     if(mode == 3) {
-        bus_format = MEDIA_BUS_FMT_RGB565_1X24_CPADHI;
+        bus_format = 0x1020;
+        //MEDIA_BUS_FMT_RGB565_1X24_CPADHI in kernel 5.15;
     }
 
     ret = drm_display_info_set_bus_formats(&vga->connector.display_info,
